da1c7df69e3b5ff03cde1e24e4aef9b3cbbb619b
spectocr <highstakes22@gmail.com>
2021-06-13 18:08:56 -0400

    yass finished 10.2

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,10 +1,8 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion.js');
-
-console.log(new Potion());
 
 test('create a player object', () => {
     const player = new Player('Dave');
@@ -14,4 +12,23 @@ test('create a player object', () => {
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]))
+});
+
+test("gets player's status as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
